0395755b111e305b9a0e066c3d60af84a8306833
boytrend <boytrend@gmail.com>
2023-01-10 14:59:11 +0400

    get token

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,4 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://164.92.178.197:4050/api/v1/sign-up/'
-// axios.defaults.baseURL = 'http://164.92.178.197:4050/api/v1/test/'
-// axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com/posts'
+axios.defaults.baseURL = 'http://164.92.178.197/sign-up'
 export default axios
